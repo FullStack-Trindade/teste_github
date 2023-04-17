4bf3aab3e6f173b4d102ad93e4c68e695fea872c
douglas-cavalcante <douglascavalcante2803@gmail.com.br>
2023-04-17 20:26:30 -0300

    adiciona criacao do model de veterinario

---
+++ src/models/veterario.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+const conexao = require('../database')
+
+const Veterinario = conexao.define('vetirinario', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+     },
+     nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+     },
+     data_de_adocao: {
+        type: Sequelize.DATEONLY
+     },
+     lista_de_alergias: {
+        type: Sequelize.STRING
+     },
+     raca: {
+        type: Sequelize.ENUM("CARAMELO", "DOBERMAN", "GUAPECA")
+     },
+     idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+     },
+     status: {
+      type: Sequelize.ENUM("SAUDAVEL", "EM_RECUPERACAO", "ENFERMO"),
+      defaultValue: 'SAUDAVEL'
+     }
+})
+
+module.exports = Veterinario
